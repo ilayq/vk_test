587bf07005867bb12cf26dcf4fdcbdbcfa8c4a92
misha <ilayfeeed@gmail.com>
2024-04-29 17:30:08 +0300

    change host to db_host

--- src/ORM.py
+++ src/ORM.py
@@ -4,8 +4,8 @@ import os
 
 db_user = os.getenv('POSTGRES_USER')
 db_user_password = os.getenv('POSTGRES_PASSWORD')
-host = os.getenv('HOST') or 'postgres'
-db_name = os.getenv('db_name') or 'vk_test'
+host = os.getenv('DB_HOST') or 'postgres'
+db_name = os.getenv('DB_NAME') or 'vk_test'
 db_connection_string = f'postgresql+asyncpg://{db_user}:{db_user_password}@{host}:5432/{db_name}'
 
 engine = create_async_engine(db_connection_string)
