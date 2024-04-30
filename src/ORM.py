98499eb9c5f94fba760140fbe4d96330fe2605e0
misha <ilayfeeed@gmail.com>
2024-04-30 13:53:57 +0300

    docker-compose final version

--- src/ORM.py
+++ src/ORM.py
@@ -5,7 +5,7 @@ import os
 db_user = os.getenv('POSTGRES_USER')
 db_user_password = os.getenv('POSTGRES_PASSWORD')
 host = os.getenv('HOST') or 'localhost'
-db_name = os.getenv('DB_NAME') or 'vk_test'
+db_name = os.getenv('POSTGRES_DB') or 'vk_test'
 db_connection_string = f'postgresql+asyncpg://{db_user}:{db_user_password}@{host}:5432/{db_name}'
 
 engine = create_async_engine(db_connection_string)
